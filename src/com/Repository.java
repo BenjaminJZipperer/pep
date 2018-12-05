7ee9725b47ea6b8ed3998e4c66ef9731a4b75144
Benjamin Zipperer <benjaminz.1@gmx.de>
2018-12-05 09:55:43 +0100

    Add 1 works

--- src/com/Repository.java
+++ src/com/Repository.java
@@ -14,6 +14,7 @@ private static List <User> users = new ArrayList <User> ();
 public static DB db;
 public static DBCollection coll;
 
+static BasicDBObject doc1 = new BasicDBObject(); // works like a HashMap ; using put (key,value);
 public static boolean init() throws UnknownHostException
 {
 	try
@@ -27,8 +28,8 @@ public static boolean init() throws UnknownHostException
 	    	 System.out.println("Datenbank-Referenz wurde erzeugt.");
 	    System.out.println("Datenbankname ["+db.toString()+"]");
 	    
-	    coll = db.createCollection("useritem",null);
-
+	    coll = db.createCollection("useritem",null); // where useritem = collectionname
+	    
 	}
 	catch(Exception exeConnect)
 	{
@@ -41,9 +42,16 @@ static void fillRepository()
 	// folgende
 	System.out.println("folgende ELemente sind im Repository");
 	users = Hauptapp.testArray();
+	
 }
 static void saveRepository()
 {
-	
+	for (User u : users)
+	{
+		doc1.append("username",u.getUsername())
+		.append("password", u.getPassword())
+		.append("personal-id", u.getFid());
+		coll.insert(doc1);
+	}
 }
 }
